ada8d5a83a90f2c48ec5df94ea8d6f79de7067d5
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-10 15:17:25 +0900

    ModalScene
    complete

---
+++ p44/ModalViewController.h
@@ -0,0 +1,14 @@
+//
+//  ModalViewController.h
+//  p44
+//
+//  Created by SDT-1 on 2014. 1. 10..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *modalInput;
+@property(strong, nonatomic) NSString*msg;
+@end
